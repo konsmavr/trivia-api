62d67308406efaa4327bacf430bf9053a18a916b
KONSTANTINOS MAVRIDIS <kmauridis@net.geoanalysis.gr>
2023-02-15 15:15:51 +0200

    app is ready

---
+++ trivia/views.py
@@ -0,0 +1,12 @@
+from rest_framework import viewsets
+
+from .models import Question
+from .serializers import QuestionSerializer
+
+
+class QuestionViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+
+
